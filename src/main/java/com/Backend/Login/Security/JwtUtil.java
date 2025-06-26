218d43add39384f8570fd32d6957b89762289e20
Aakriti Singh <aakritisingh2804@gmail.com>
2025-06-26 20:47:37 +0530

    Updated JwtUtil with valid key

--- src/main/java/com/Backend/Login/Security/JwtUtil.java
+++ src/main/java/com/Backend/Login/Security/JwtUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,12 +20,18 @@ import java.util.function.Function;
 
 @Component
 public class JwtUtil {
+
     @Value("${jwt.secret}")
     private String SECRET_KEY;
 
-    Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    private Key key;
 
     private static final long EXPIRATION_TIME = 1000 * 60 * 60;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
 
 
     public String generateToken(String email, String name, String role) {
